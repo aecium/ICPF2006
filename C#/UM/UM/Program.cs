7c9271f92785094c57d153d10364ce45107bb9cd
aecium <aecium@gmail.com>
2018-08-21 19:14:12 -0500

    clean up of some debuging code, gave another slight speed boost

--- C#/UM/UM/Program.cs
+++ C#/UM/UM/Program.cs
@@ -46,7 +46,6 @@ namespace UM
 			}
 			catch (IOException e)
 			{
-				// TODO Auto-generated catch block();
 				System.Diagnostics.Debug.WriteLine(e.StackTrace);
 			}
 
@@ -99,7 +98,6 @@ namespace UM
 						break;
 					// 7 Halt
 					case 7:
-						//Console.Out.WriteLine("Halt");
 						run = false;
 						break;
 					// 8 Allocation
@@ -108,7 +106,6 @@ namespace UM
 						break;
 					// 9 Abandonment
 					case 9:
-						//Console.Out.WriteLine("Abandonment");
 						abandonment();
 						break;
 					// 10 Output 
@@ -130,12 +127,6 @@ namespace UM
 				}
 
 				iFinger++;
-
-				if (iFinger > arrayCollection[0].Count)
-				{
-					//System.out.println("Stoped unexpected EOF");
-					break;
-				}
 
 			}
 		}
@@ -175,7 +166,6 @@ namespace UM
 
 		private static void conditionalMove()
 		{
-			//Console.Out.WriteLine("Conditional Move");
 			if (GPR[regC] != 0)
 			{
 				GPR[regA] = GPR[regB];
@@ -184,51 +174,43 @@ namespace UM
 
 		private static void arrayIndex()
 		{
-			//Console.Out.WriteLine("Array Index");
 			GPR[regA] = arrayCollection[(int)GPR[regB]][(int)GPR[regC]];
 		}
 
 		private static void amendment()
 		{
-			//Console.Out.WriteLine("Array Amendment");
 			arrayCollection[(int)GPR[regA]][(int)GPR[regB]] = GPR[regC];
 		}
 
 		private static void addition()
 		{
-			//Console.Out.WriteLine("Addition");
 			GPR[regA] = (uint)((GPR[regB] + GPR[regC]) % (long)Math.Pow(2, 32));
 		}
 
 		private static void multiplication()
 		{
-			//Console.Out.WriteLine("Multiplication");
 			GPR[regA] = GPR[regA] = (uint)((GPR[regB] * GPR[regC]) % (long)Math.Pow(2, 32));
 		}
 
 		private static void division()
 		{
-			//Console.Out.WriteLine("Division");
 			GPR[regA] = (uint)Math.Floor((double)(GPR[regB] / GPR[regC]));
 		}
 
 		private static void notAnd()
 		{
-			//Console.Out.WriteLine("Not And");
 			GPR[regA] = ~(GPR[regB] & GPR[regC]);
 		}
 
 		private static void abandonment()
 		{
 			arrayCollection[(int)GPR[regC]] = null;
-			//arrayColectionSize--;
 		}
 
 		private static void allocation()
 		{
-			//Console.Out.WriteLine("Allocation");
 			arrayCollection.Add(new List<uint>());
-			arrayColectionSize++; //arrayCollection.Count - 1;
+			arrayColectionSize++;
 			for (int i = 1; i <= GPR[regC]; i++)
 			{
 				arrayCollection[arrayColectionSize].Add(0);
@@ -238,7 +220,6 @@ namespace UM
 
 		private static void loadProgram()
 		{
-			//Console.Out.WriteLine("Load Program " + GPR[regB] + " " + GPR[regC] );
 			if (!GPR[regB].Equals(0))
 			{
 				arrayCollection[0] = arrayCollection[(int)GPR[regB]].GetRange(0, arrayCollection[(int)GPR[regB]].Count);
@@ -248,13 +229,11 @@ namespace UM
 
 		private static void orthopraphy()
 		{
-			//Console.Out.WriteLine("Orthography");
-			A13 =  (bits << 4) >> 29;
-			val13 =  (bits << 7) >> 7;
+			A13 = (bits << 4) >> 29;
+			val13 = (bits << 7) >> 7;
 			GPR[A13] = val13;
-			////Console.Out.WriteLine(A13 + " " + val13);
 		}
-              
+
 		private static long byteAsULong(byte b)
 		{
 			return ((long)b) & 0x00000000000000FFL;
